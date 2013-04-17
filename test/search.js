cd99bfc9fc6166e54c7cb7f71ee1d1bfc0b74023
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-17 19:37:33 +0100

    test for real-time search

---
+++ test/search.js
@@ -0,0 +1,48 @@
+
+var nonsense = require('nonsense')
+var levelup  = require('levelup')
+var sublevel = require('level-sublevel')
+
+var dir = '/tmp/test-inverted-index'
+require('rimraf').sync(dir)
+
+var db       = sublevel(levelup(dir))
+var hash     = require('sha1sum')
+var InvertedIndex
+             = require('../')
+var fs       = require('fs')
+var path     = require('path')
+
+var indexDb = InvertedIndex(db, 'index')
+
+
+require('tape')(function (t) {
+
+  t.plan(10)
+
+  var r = nonsense.sentences(
+    fs.createReadStream(path.join(__dirname, 'art-of-war.txt'))
+  , 2)
+  .on('end', function () {
+
+    var int = 
+    setInterval(function () {
+      var line = r.random().join(' ')
+      db.put(Date.now(), line, function (err) {
+        if(err) throw err
+      })
+    }, 20)
+
+    var n = 10
+    indexDb.createQueryStream(['w~'], {tail: true})
+    .on('data', function (d) {
+     console.log(d)
+      db.get(d.key, function (err, value) {
+        t.ok(value.indexOf(' w'))
+        if(--n) return
+        clearInterval(int)
+        t.end()
+      })
+    })
+  })
+})
